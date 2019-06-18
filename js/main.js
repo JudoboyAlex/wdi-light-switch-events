e4f8dc3b2960e278fa090c2fb92ddc75e9c09617
Alexander G. Yang <spark8402@rogers.com>
2019-06-18 15:10:05 -0400

    assignment done

--- js/main.js
+++ js/main.js
@@ -1,2 +1,36 @@
+// Get some handles to elements on the DOM
+const toggleSwitch = document.querySelector(".switch");
+const backgroundColorSwitch = document.querySelector(".light");
+const textChg = document.querySelector("h1");
 
 
+// Define state transformations
+function toggleLight() {
+  // If light is on, turn it off
+ 
+  if (toggleSwitch.classList.contains("on")) {
+
+    toggleSwitch.classList.remove("on");
+    toggleSwitch.classList.add("off");
+
+    backgroundColorSwitch.classList.remove("light");
+    backgroundColorSwitch.classList.add("dark");
+
+    textChg.innerHTML = "Hey, who turned off the lights?";
+
+  } else {
+    // Turning On
+    toggleSwitch.classList.remove("off");
+    toggleSwitch.classList.add("on");
+
+    backgroundColorSwitch.classList.remove("dark");
+    backgroundColorSwitch.classList.add("light");
+
+    textChg.innerHTML = "It's so bright in here!";
+  }
+
+ 
+}
+
+// Set up triggers for state changes
+toggleSwitch.addEventListener("click", toggleLight);
